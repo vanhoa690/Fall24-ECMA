3e97c816ae6e223280354b521ec5defacc5112d0
vanhoa690 <vanhoa690@gmail.com>
2024-11-05 16:21:56 +0700

    create server and db

---
+++ main.js
@@ -0,0 +1,46 @@
+const products = [
+  {
+    id: 1,
+    name: "Iphone 13",
+    price: 1111,
+  },
+  {
+    id: 2,
+    name: "Iphone 14",
+    price: 1111,
+  },
+  {
+    id: 3,
+    name: "Iphone 15",
+    price: 1111,
+  },
+];
+
+function renderProductRow(product) {
+  return `
+   <tr>
+              <th scope="row">${product.id}</th>
+              <td>${product.name}</td>
+              <td>${product.price} VND</td>
+              <td>
+                <button class="btn btn-danger">Delete</button>
+              </td>
+            </tr>
+  `;
+}
+
+document.getElementById("list").innerHTML = `
+<table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        ${products.map(renderProductRow).join("")}
+        </tbody>
+      </table>
+`;
