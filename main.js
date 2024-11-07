300db30c58165ffd92bb96e1767202c5d2ed1858
vanhoa690 <vanhoa690@gmail.com>
2024-11-07 19:19:00 +0700

    map js

--- main.js
+++ main.js
@@ -4,6 +4,42 @@ const product = {
   price: 100,
 };
 
+const products = [
+  {
+    id: 1,
+    name: "Iphone 13",
+    price: 100,
+  },
+  {
+    id: 2,
+    name: "Iphone 14",
+    price: 100,
+  },
+  {
+    id: 3,
+    name: "Iphone 15",
+    price: 100,
+  },
+  {
+    id: 3,
+    name: "Iphone 15",
+    price: 100,
+  },
+];
+
+function renderProductRow(product) {
+  return `
+   <tr>
+            <th scope="row">${product.id}</th>
+            <td>${product.name}</td>
+            <td>${product.price} USD</td>
+            <td>
+              <button class="btn btn-danger">Delete</button>
+            </td>
+          </tr>
+  `;
+}
+
 document.getElementById("list").innerHTML = `
  <table class="table">
         <thead>
@@ -15,14 +51,7 @@ document.getElementById("list").innerHTML = `
           </tr>
         </thead>
         <tbody>
-        <tr>
-            <th scope="row">${product.id}</th>
-            <td>${product.name}</td>
-            <td>${product.price} USD</td>
-            <td>
-              <button class="btn btn-danger">Delete</button>
-            </td>
-          </tr>
+          ${products.map(renderProductRow).join("")}
         </tbody>
       </table>
 
