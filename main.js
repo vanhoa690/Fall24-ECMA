92b06017ddc7b323bcbc465729cb7b227a9f732c
vanhoa690 <vanhoa690@gmail.com>
2024-11-07 16:27:14 +0700

    update

--- main.js
+++ main.js
@@ -11,39 +11,10 @@ function renderProductRow(product) {
   `;
 }
 
-// () => {}
-function getProductFetch() {
-  fetch("http://localhost:3000/products")
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      document.getElementById("list").innerHTML = `
-          <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">Price</th>
-                <th scope="col">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-            ${data.map(renderProductRow).join("")}
-            </tbody>
-          </table>
-    `;
-    })
-    .catch();
-}
-
-// getProductFetch();
-
 function getProductAxios() {
   axios
     .get("http://localhost:3000/products")
     .then((res) => {
-      console.log(res.data);
       const products = res.data;
       document.getElementById("list").innerHTML = `
           <table class="table">
