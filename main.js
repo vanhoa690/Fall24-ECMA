e0823acdce0285d8e4d26075f35f5fea09aa9aaf
vanhoa690 <vanhoa690@gmail.com>
2024-11-09 18:12:27 +0700

    backup

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "Iphone 13",
+    price: 100,
+  },
+  {
+    id: 2,
+    name: "Iphone 14",
+    price: 100,
+  },
+  {
+    id: 3,
+    name: "Iphone 15",
+    price: 100,
+  },
+  {
+    id: 3,
+    name: "Iphone 15",
+    price: 100,
+  },
+];
 function renderProductRow(product) {
   return `
    <tr>
@@ -11,55 +33,18 @@ function renderProductRow(product) {
   `;
 }
 
-function getProductFetch() {
-  fetch("http://localhost:3000/products")
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      document.getElementById("list").innerHTML = `
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Price</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${data.map(renderProductRow).join("")}
-        </tbody>
-      </table>
+document.getElementById("list").innerHTML = `
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Price</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    ${products.map(renderProductRow).join("")}
+  </tbody>
+</table>
 `;
-    })
-    .catch();
-}
-
-// getProductFetch();
-
-function getProductAxios() {
-  axios
-    .get("http://localhost:3000/products")
-    .then((res) => {
-      const products = res.data;
-      document.getElementById("list").innerHTML = `
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Price</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${products.map(renderProductRow).join("")}
-        </tbody>
-      </table>
-`;
-    })
-    .catch();
-}
-
-getProductAxios();
