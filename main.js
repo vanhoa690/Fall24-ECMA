d7fb1aedb13ebf95f9cbc5a4b9cec182f3f3bd22
vanhoa690 <vanhoa690@gmail.com>
2024-11-09 19:55:05 +0700

    axios product list

--- main.js
+++ main.js
@@ -59,4 +59,30 @@ function getProductFetch() {
     })
     .catch();
 }
-getProductFetch();
+// getProductFetch();
+
+function getProductAxios() {
+  axios
+    .get("http://localhost:3000/products")
+    .then((res) => {
+      const products = res.data;
+      document.getElementById("list").innerHTML = `
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${products.map(renderProductRow).join("")}
+        </tbody>
+      </table>
+`;
+    })
+    .catch();
+}
+
+getProductAxios();
