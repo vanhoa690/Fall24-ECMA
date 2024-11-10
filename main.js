344cc809c79a4d0ea2eba9b44f817f181f12368e
vanhoa690 <vanhoa690@gmail.com>
2024-11-10 19:10:59 +0700

    done product list

--- main.js
+++ main.js
@@ -11,61 +11,9 @@ function renderProductRow(product) {
   `;
 }
 
-function getProductFetch() {
-  fetch("http://localhost:3000/products")
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      document.getElementById("list").innerHTML = `
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Name</th>
-              <th scope="col">Price</th>
-              <th scope="col">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            ${data.map(renderProductRow).join("")}
-          </tbody>
-        </table>
-`;
-    })
-    .catch();
-}
-// getProductFetch();
-
-function getProductAxios() {
-  axios
-    .get("http://localhost:3000/products")
-    .then((res) => {
-      const products = res.data;
-      document.getElementById("list").innerHTML = `
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Price</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${products.map(renderProductRow).join("")}
-        </tbody>
-      </table>
-`;
-    })
-    .catch();
-}
-
-// getProductAxios();
-
-async function getProductAsyncFetch() {
-  const res = await fetch("http://localhost:3000/products");
-  const data = await res.json();
+async function showProductList() {
+  const res = await axios.get("http://localhost:3000/products");
+  const products = res.data;
   document.getElementById("list").innerHTML = `
   <table class="table">
     <thead>
@@ -77,10 +25,10 @@ async function getProductAsyncFetch() {
       </tr>
     </thead>
     <tbody>
-      ${data.map(renderProductRow).join("")}
+      ${products.map(renderProductRow).join("")}
     </tbody>
   </table>
 `;
 }
 
-getProductAsyncFetch();
+showProductList();
