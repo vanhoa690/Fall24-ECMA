c670b586bac73f7a90fef210863af5d31beda5a7
vanhoa690 <vanhoa690@gmail.com>
2024-11-11 17:41:31 +0700

    add alert

--- add.js
+++ add.js
@@ -1,11 +1,16 @@
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
 
   const productName = document.getElementById("name").value;
   const productPrice = document.getElementById("price").value;
 
-  axios.post("http://localhost:3000/products", {
-    name: productName,
-    price: productPrice,
-  });
+  try {
+    await axios.post("http://localhost:3000/products", {
+      name: productName,
+      price: productPrice,
+    });
+    alert("them thanh cong");
+  } catch (error) {
+    alert("Error");
+  }
 }
