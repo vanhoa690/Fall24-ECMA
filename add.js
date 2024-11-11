9d5b2a9c5bcfb05132362d9554f2e40bef0830d1
vanhoa690 <vanhoa690@gmail.com>
2024-11-11 18:09:53 +0700

    backup

--- add.js
+++ add.js
@@ -1,17 +1,1 @@
-async function handleSubmit(event) {
-  event.preventDefault();
-
-  const productName = document.getElementById("name").value;
-  const productPrice = document.getElementById("price").value;
-
-  try {
-    await axios.post("http://localhost:3000/products", {
-      name: productName,
-      price: productPrice,
-    });
-    location.href = "/";
-    alert("them thanh cong");
-  } catch (error) {
-    alert("Error");
-  }
-}
+console.log("Add Product");
