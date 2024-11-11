bc55c70e8ac9ea0b81520cd2b329dc3c53fa351a
vanhoa690 <vanhoa690@gmail.com>
2024-11-11 17:32:44 +0700

    refactor

--- add.js
+++ add.js
@@ -1,14 +1,9 @@
 function handleSubmit(event) {
-  // Ngăn chặn trang tải lại
   event.preventDefault();
 
-  // bat du lieu gui len
   const productName = document.getElementById("name").value;
   const productPrice = document.getElementById("price").value;
 
-  console.log("product name", productName);
-
-  // call api axios.post()
   axios.post("http://localhost:3000/products", {
     name: productName,
     price: productPrice,
