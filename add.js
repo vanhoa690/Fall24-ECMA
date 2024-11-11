e4dfb4c7d965f768feef990b1e81713ff67e365c
vanhoa690 <vanhoa690@gmail.com>
2024-11-11 17:01:42 +0700

    add submit

--- add.js
+++ add.js
@@ -1,1 +1,10 @@
-console.log("Add product");
+function handleSubmit(event) {
+  // Ngăn chặn trang tải lại
+  event.preventDefault();
+
+  // bat du lieu gui len
+  const productName = document.getElementById("name").value;
+
+  console.log("product name", productName);
+}
+// call api axios.post()
