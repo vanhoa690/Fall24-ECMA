dcbf9f7bf62470644944165f1a9708acb8297f50
vanhoa690 <vanhoa690@gmail.com>
2024-11-12 17:27:19 +0700

    add price

--- add.js
+++ add.js
@@ -1,11 +1,11 @@
 function handleSubmit(event) {
-  // Ngăn chặn trang tải lại
   event.preventDefault();
-  const productName = document.getElementById("name").value;
-  console.log("productName", productName);
 
-  // call api
+  const productName = document.getElementById("name").value;
+  const productPrice = document.getElementById("price").value;
+
   axios.post("http://localhost:3000/products", {
     name: productName,
+    price: productPrice,
   });
 }
