72cb9300c6662034cc079868480e48c02dd2c820
vanhoa690 <vanhoa690@gmail.com>
2024-11-12 19:17:52 +0700

    add price

--- add.js
+++ add.js
@@ -3,4 +3,8 @@ function handleSubmit(event) {
   const productName = document.getElementById("name").value;
 
   console.log("product name", productName);
+  axios.post("http://localhost:3000/products", {
+    name: productName,
+    price: 1,
+  });
 }
