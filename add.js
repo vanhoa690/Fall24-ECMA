1498a042c97c94861641afef7bc98d757ce0a804
vanhoa690 <vanhoa690@gmail.com>
2024-11-12 17:31:21 +0700

    done create

--- add.js
+++ add.js
@@ -1,11 +1,18 @@
-function handleSubmit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
 
   const productName = document.getElementById("name").value;
   const productPrice = document.getElementById("price").value;
 
-  axios.post("http://localhost:3000/products", {
-    name: productName,
-    price: Number(productPrice),
-  });
+  try {
+    await axios.post("http://localhost:3000/products", {
+      name: productName,
+      price: Number(productPrice),
+    });
+    location.href = "/";
+
+    alert("them thanh cong");
+  } catch (error) {
+    alert("error");
+  }
 }
