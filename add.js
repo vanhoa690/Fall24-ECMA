07853f38b22b4f9a466a9c9fb768548b9f922526
vanhoa690 <vanhoa690@gmail.com>
2024-11-13 17:28:56 +0700

    done update

--- add.js
+++ add.js
@@ -1,9 +1,9 @@
-console.log(location.search.split("=")[1]);
+const productId = location.search.split("=")[1];
 
 async function getProductDetail() {
-  const productId = location.search.split("=")[1];
+  if (!productId) return;
+
   const res = await axios.get(`http://localhost:3000/products/${productId}`);
-  console.log(res);
 
   document.getElementById("name").value = res.data.name;
   document.getElementById("price").value = res.data.price;
@@ -13,7 +13,7 @@ getProductDetail();
 
 async function handleSubmit(event) {
   event.preventDefault();
-  const productId = location.search.split("=")[1];
+
   const productName = document.getElementById("name").value;
   const productPrice = document.getElementById("price").value;
   const data = {
