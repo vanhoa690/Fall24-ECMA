3a657fd86eb10c90041de283227128c952213260
vanhoa690 <vanhoa690@gmail.com>
2024-11-14 19:12:40 +0700

    get product detail

--- add.js
+++ add.js
@@ -1,3 +1,12 @@
+const productId = location.search.split("=")[1];
+
+async function getProductDetail() {
+  const res = await axios.get(`http://localhost:3000/products/${productId}`);
+  console.log(res);
+}
+
+getProductDetail();
+
 async function handleSubmit(event) {
   event.preventDefault();
   const productName = document.getElementById("name").value;
