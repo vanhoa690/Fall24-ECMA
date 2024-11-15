70d1f8bbf6acdb62491748d3038943c7babd637e
vanhoa690 <vanhoa690@gmail.com>
2024-11-15 17:43:22 +0700

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useRoutes } from "react-router-dom";
 import ProductList from "./pages/ProductList";
+import ProductAdd from "./pages/ProductAdd";
 
 function App() {
   const routes = [
     {
       path: "/product/list",
       element: <ProductList />,
+    },
+    {
+      path: "/product/add",
+      element: <ProductAdd />,
+    },
+    {
+      path: "/product/edit/:id",
+      element: <ProductEdit />,
     },
   ];
   return <div></div>;
