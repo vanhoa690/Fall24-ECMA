aabf2f55fd8a019ea947a9a32cb8607efee01e6d
vanhoa690 <vanhoa690@gmail.com>
2024-11-15 17:08:28 +0700

    remove code

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,10 @@ function App() {
     alert(myName);
   }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div>
       <h1>{myName}</h1>
       <button onClick={sayHello}>Say Hello</button>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
