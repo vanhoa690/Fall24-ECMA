307382a8436c5d23672d068875e630167fad9c41
vanhoa690 <vanhoa690@gmail.com>
2024-11-19 20:15:16 +0700

    add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useRoutes } from "react-router-dom";
 import ProductAdd from "./pages/ProductAdd";
 import ProductEdit from "./pages/ProductEdit";
 import ProductList from "./pages/ProductList";
+import Homepage from "./pages/Homepage";
 
 function App() {
   const routes = [
+    {
+      path: "/",
+      element: <Homepage />,
+    },
     {
       path: "product/list",
       element: <ProductList />,
