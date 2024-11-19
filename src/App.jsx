38054d4db3f7fe81f1db5bf3e081aca2ecf42882
vanhoa690 <vanhoa690@gmail.com>
2024-11-19 17:50:44 +0700

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useRoutes } from "react-router-dom";
+import ProductAdd from "./pages/ProductAdd";
+import ProductEdit from "./pages/ProductEdit";
+import ProductList from "./pages/ProductList";
 
 function App() {
-  const [count, setCount] = useState(0);
-  // js
-  const myName = "hoadv";
-  function sayHello() {
-    alert(myName);
-  }
-  return (
-    <div>
-      <h2>My name is {myName} </h2>
-      <button onClick={sayHello}>say hello</button>
-    </div>
-  );
+  const routes = [
+    {
+      path: "product/list",
+      element: <ProductList />,
+    },
+    {
+      path: "product/add",
+      element: <ProductAdd />,
+    },
+    {
+      path: "product/edit/:id",
+      element: <ProductEdit />,
+    },
+  ];
+
+  const element = useRoutes(routes);
+  
+  return <main>{element}</main>;
 }
 
 export default App;
