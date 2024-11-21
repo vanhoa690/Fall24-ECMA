04302110cfd837993d2baca3688fd57afcea3f2c
vanhoa690 <vanhoa690@gmail.com>
2024-11-21 16:58:24 +0700

    map product list

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,34 +1,47 @@
 function ProductList() {
+  const products = [
+    {
+      id: 1,
+      name: "Smartphone",
+      price: 800,
+    },
+    {
+      id: 2,
+      name: "Smartphone",
+      price: 800,
+    },
+    {
+      name: "Tablet 16 update",
+      price: 900,
+      id: 4,
+    },
+  ];
   return (
     <div className="container">
       <h1 className="text-center my-2">ProductList</h1>
       <table className="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {products.map((product) => {
+            return (
+              <tr>
+                <th scope="row">{product.id}</th>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>
+                  <button className="btn btn-info">Edit</button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
