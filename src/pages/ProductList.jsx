c22b9add968d21401126a151c1b6bc395484cd66
vanhoa690 <vanhoa690@gmail.com>
2024-11-21 19:00:33 +0700

    add products

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,14 +1,31 @@
 function ProductList() {
+  const products = [
+    {
+      id: 1,
+      name: "Smartphone",
+      price: 800,
+    },
+    {
+      id: 2,
+      name: "Smartphone",
+      price: 800,
+    },
+    {
+      name: "Tablet 16 update",
+      price: 900,
+      id: 4,
+    },
+  ];
   return (
     <div className="container">
       <h1 className="text-center my-2">ProductList</h1>
-      <table class="table">
+      <table className="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -16,7 +33,10 @@ function ProductList() {
             <th scope="row">1</th>
             <td>Mark</td>
             <td>Otto</td>
-            <td>@mdo</td>
+            <td>
+              <button className="btn btn-danger">Delete</button>
+              <button className="btn btn-info">Edit</button>
+            </td>
           </tr>
           <tr>
             <th scope="row">2</th>
