d1e8c3bec767c83bab38fc3d3303ba6441979d8b
vanhoa690 <vanhoa690@gmail.com>
2024-11-21 19:09:48 +0700

    map product list

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -29,26 +29,19 @@ function ProductList() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>
-              <button className="btn btn-danger">Delete</button>
-              <button className="btn btn-info">Edit</button>
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {products.map((product) => {
+            return (
+              <tr>
+                <th scope="row">{product.id}</th>
+                <td>{product.name}</td>
+                <td>{product.price} USD</td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>
+                  <button className="btn btn-info">Edit</button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
