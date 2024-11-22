12d365d3cf545a9bf8cdc7fdac3f8aeafba1e339
vanhoa690 <vanhoa690@gmail.com>
2024-11-22 16:27:34 +0700

    product list

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,35 @@ import { useRoutes } from "react-router-dom";
 import ProductAdd from "./pages/ProductAdd";
 import ProductEdit from "./pages/ProductEdit";
 import ProductList from "./pages/ProductList";
+import Homepage from "./pages/Homepage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const routes = [
-    { path: "product/list", element: <ProductList /> },
-    { path: "product/add", element: <ProductAdd /> },
-    { path: "product/edit/:id", element: <ProductEdit /> },
+    {
+      path: "/",
+      element: <Homepage />,
+    },
+    {
+      path: "product/list",
+      element: <ProductList />,
+    },
+    {
+      path: "product/add",
+      element: <ProductAdd />,
+    },
+    {
+      path: "product/edit/:id",
+      element: <ProductEdit />,
+    },
   ];
 
   const element = useRoutes(routes);
-
-  return <div>{element}</div>;
+  return (
+    <main>
+      <Toaster />
+      {element}
+    </main>
+  );
 }
-
 export default App;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,32 +1,30 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 function ProductList() {
-  // map render product list
-  const products = [
-    {
-      id: 1,
-      name: "Smartphone",
-      price: 800,
-    },
-    {
-      id: 2,
-      name: "Laptop",
-      price: 800,
-    },
-    {
-      name: "Tablet",
-      price: 600,
-      id: 3,
-    },
-  ];
+  const [products, setProducts] = useState([]);
 
   async function getProductList() {
     const res = await axios.get("http://localhost:3000/products");
-    console.log(res);
+    setProducts(res.data);
   }
 
-  getProductList();
+  async function deleteProduct(id) {
+    if (confirm("Di choi ko")) {
+      try {
+        await axios.delete(`http://localhost:3000/products/${id}`);
+        toast.success("Xoa thanh cong");
+      } catch (error) {
+        console.log(error);
+        toast.error("Error");
+      }
+    }
+  }
 
+  useEffect(() => {
+    getProductList();
+  }, []);
   return (
     <div className="container">
       <h1 className="text-center my-2">ProductList</h1>
@@ -40,17 +38,19 @@ function ProductList() {
           </tr>
         </thead>
         <tbody>
-          {products.map((product) => (
-            <tr>
-              <th scope="row">{product.id}</th>
-              <td>{product.name}</td>
-              <td>{product.price} USD</td>
-              <td>
-                <button className="btn btn-danger">Delete</button>
-                <button className="btn btn-info">Edit</button>
-              </td>
-            </tr>
-          ))}
+          {products.map((product, index) => {
+            return (
+              <tr key={index}>
+                <th scope="row">{product.id}</th>
+                <td>{product.name}</td>
+                <td>{product.price} USD</td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>
+                  <button className="btn btn-info">Edit</button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
