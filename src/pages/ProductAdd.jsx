040787b95c1ccbc5f39e96b2c08eefb70ae43ead
vanhoa690 <vanhoa690@gmail.com>
2024-11-22 17:15:57 +0700

    test api

--- src/pages/ProductAdd.jsx
+++ src/pages/ProductAdd.jsx
@@ -1,8 +1,25 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+
 function ProductAdd() {
+  // js
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const data = {
+      name: "Iphone 16",
+      price: 100,
+    };
+    try {
+      await axios.post("http://localhost:3000/products", data);
+      toast.success("Them thanh cong");
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div>
       <h1>ProductAdd</h1>
-      <form>
+      <form onSubmit={(event) => handleSubmit(event)}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">
             Name
