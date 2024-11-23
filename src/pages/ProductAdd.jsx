bafaa95ec7cecab0661247b7fa346952cfc6bfb1
vanhoa690 <vanhoa690@gmail.com>
2024-11-23 16:47:18 +0700

    add UI form

--- src/pages/ProductAdd.jsx
+++ src/pages/ProductAdd.jsx
@@ -1,5 +1,26 @@
 function ProductAdd() {
-  return <div>ProductAdd</div>;
+  return (
+    <div>
+      <h1>ProductAdd</h1>
+      <form>
+        <div class="mb-3">
+          <label for="name" class="form-label">
+            Name
+          </label>
+          <input type="text" class="form-control" id="name" />
+        </div>
+        <div class="mb-3">
+          <label for="price" class="form-label">
+            Price
+          </label>
+          <input type="number" class="form-control" id="price" />
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default ProductAdd;
