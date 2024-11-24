3f6173ccb946530749efd91141f7e34a723f591e
vanhoa690 <vanhoa690@gmail.com>
2024-11-24 19:04:58 +0700

    useParams

--- src/pages/ProductEdit.jsx
+++ src/pages/ProductEdit.jsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 function ProductEdit() {
+  const params = useParams();
+  console.log(params);
+
   return (
     <div>
-      <h1>Product Edit</h1>
+      <h1>Product Edit ID = {params.id}</h1>
     </div>
   );
 }
