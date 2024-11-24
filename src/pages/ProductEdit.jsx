2dc7bd5ded299cf61bc33aaf246f20b7e5bea177
vanhoa690 <vanhoa690@gmail.com>
2024-11-24 17:34:51 +0700

    copy add sang edit

--- src/pages/ProductEdit.jsx
+++ src/pages/ProductEdit.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { useParams } from "react-router-dom";
 
 function ProductEdit() {
@@ -8,7 +10,6 @@ function ProductEdit() {
 
   async function getProductDetail(id) {
     const res = await axios.get(`http://localhost:3000/products/${id}`);
-    console.log(res);
     setProduct(res.data);
   }
 
@@ -16,10 +17,67 @@ function ProductEdit() {
     getProductDetail(params.id);
   }, []);
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  async function onSubmit(data) {
+    try {
+      await axios.put(`http://localhost:3000/products/${params.id}`, data);
+
+      toast.success("them thanh cong");
+      // chuyen trang list
+    } catch (error) {
+      toast.error("error");
+    }
+  }
+
   return (
     <div>
-      <h1>ProductEdit: ID la bao nhieu? {params.id}</h1>
-      <h2>Product name: {product?.name} </h2>
+      <h1>Product Edit</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            {...register("name", {
+              required: "Name is required",
+            })}
+          />
+          {errors?.name && (
+            <small className="text-danger">{errors.name.message}</small>
+          )}
+        </div>
+        <div className="mb-3">
+          <label htmlFor="price" className="form-label">
+            Price
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="price"
+            {...register("price", {
+              required: "Price is required",
+              min: {
+                value: 0,
+                message: "Price > 0",
+              },
+            })}
+          />
+          {errors?.price && (
+            <small className="text-danger">{errors.price.message}</small>
+          )}
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
