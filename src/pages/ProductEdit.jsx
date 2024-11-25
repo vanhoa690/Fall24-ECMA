95a149112beb5c5b0e5ec8c2a039810267f8fe48
vanhoa690 <vanhoa690@gmail.com>
2024-11-25 17:33:56 +0700

    getProductDetail

--- src/pages/ProductEdit.jsx
+++ src/pages/ProductEdit.jsx
@@ -1,5 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function ProductEdit() {
-  return <div>ProductEdit</div>;
+  const params = useParams();
+  console.log(params);
+
+  const [product, setProduct] = useState();
+
+  async function getProductDetail(id) {
+    const res = await axios.get(`http://localhost:3000/products/${id}`);
+    console.log(res.data);
+    setProduct(res.data);
+  }
+
+  useEffect(() => {
+    getProductDetail(params.id);
+  }, []);
+  return (
+    <div>
+      <h1>ProductEdit co ID la {params.id}</h1>
+      <h1>ProductEdit co Product Name la {product?.name}</h1>
+    </div>
+  );
 }
 
 export default ProductEdit;
