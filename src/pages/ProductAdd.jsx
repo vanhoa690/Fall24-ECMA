3aac9a9b298880bb5ab09b88c9c56cd7aca39fdf
vanhoa690 <vanhoa690@gmail.com>
2024-11-25 18:25:41 +0700

    back ca6

--- src/pages/ProductAdd.jsx
+++ src/pages/ProductAdd.jsx
@@ -1,22 +1,17 @@
 import axios from "axios";
-import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
 
 function ProductAdd() {
-  const nav = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  async function onSubmit(data) {
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const data = {
+      name: "Iphone 16",
+      price: 100,
+    };
     try {
       await axios.post("http://localhost:3000/products", data);
       toast.success("Them thanh cong");
       // chuyen sang product list
-      nav("/product/list");
     } catch (error) {
       console.log(error);
       toast.error("Error");
@@ -26,38 +21,18 @@ function ProductAdd() {
   return (
     <div>
       <h1>ProductAdd</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={(event) => handleSubmit(event)}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            {...register("name", {
-              required: "Name is Requied",
-            })}
-          />
-          <small className="text-danger">{errors.name?.message}</small>
+          <input type="text" className="form-control" id="name" />
         </div>
         <div className="mb-3">
           <label htmlFor="price" className="form-label">
             Price
           </label>
-          <input
-            type="number"
-            className="form-control"
-            id="price"
-            {...register("price", {
-              required: "Price is Requied",
-              min: {
-                value: 0,
-                message: "Price > 0",
-              },
-            })}
-          />
-          <small className="text-danger">{errors.price?.message}</small>
+          <input type="number" className="form-control" id="price" />
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
