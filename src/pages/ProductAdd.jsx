e391c5d91889600cef878fb6babca9c9862c0e0b
vanhoa690 <vanhoa690@gmail.com>
2024-11-25 18:51:26 +0700

    add product

--- src/pages/ProductAdd.jsx
+++ src/pages/ProductAdd.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
+import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
 function ProductAdd() {
-  async function handleSubmit(event) {
-    event.preventDefault();
-    const data = {
-      name: "Iphone 16",
-      price: 100,
-    };
+  const { register, handleSubmit } = useForm();
+
+  async function onSubmit(data) {
+    console.log(data);
+
     try {
       await axios.post("http://localhost:3000/products", data);
       toast.success("Them thanh cong");
@@ -21,18 +21,28 @@ function ProductAdd() {
   return (
     <div>
       <h1>ProductAdd</h1>
-      <form onSubmit={(event) => handleSubmit(event)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input type="text" className="form-control" id="name" />
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            {...register("name")}
+          />
         </div>
         <div className="mb-3">
           <label htmlFor="price" className="form-label">
             Price
           </label>
-          <input type="number" className="form-control" id="price" />
+          <input
+            type="number"
+            className="form-control"
+            id="price"
+            {...register("price")}
+          />
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
