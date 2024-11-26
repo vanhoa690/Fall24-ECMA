65a455ec60afd4f6b1341f27bd3519a5a3a15c2f
vanhoa690 <vanhoa690@gmail.com>
2024-11-26 19:09:31 +0700

    call api

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useForm } from "react-hook-form";
 
 function Register() {
@@ -7,8 +8,9 @@ function Register() {
     formState: { errors },
   } = useForm();
 
-  function onSubmit(data) {
+  async function onSubmit(data) {
     console.log(data);
+    await axios.post("http://localhost:3000/register", data);
   }
   return (
     <div>
