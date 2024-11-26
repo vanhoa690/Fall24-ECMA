8881ce046ab56f1dc69b178c2bb4579070e92f54
vanhoa690 <vanhoa690@gmail.com>
2024-11-26 16:54:22 +0700

    add UI

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,35 @@
 function Register() {
   return (
     <div>
-      <h1>Register</h1>
+      <h1 className="text-center my-2">Register</h1>
+      <form>
+        <div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
