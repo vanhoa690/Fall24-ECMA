989ce94f40c9e94508b143923633a59ce07a7457
vanhoa690 <vanhoa690@gmail.com>
2024-11-27 19:04:20 +0700

    useform register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,19 @@
+import { useForm } from "react-hook-form";
+
 function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <div>
       <h1 className="text-center my-2">Register</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Email address
@@ -12,6 +23,9 @@ function Register() {
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            {...register("email", {
+              required: "email is required",
+            })}
           />
         </div>
         <div className="mb-3">
@@ -22,6 +36,9 @@ function Register() {
             type="password"
             className="form-control"
             id="exampleInputPassword1"
+            {...register("password", {
+              required: "password is required",
+            })}
           />
         </div>
 
