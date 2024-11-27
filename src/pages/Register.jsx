febca2ec2b99431d74b153b78b726b8fdedf0f9a
vanhoa690 <vanhoa690@gmail.com>
2024-11-27 19:22:09 +0700

    validate required

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 function Register() {
   const {
@@ -6,8 +8,14 @@ function Register() {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  function onSubmit(data) {
+  async function onSubmit(data) {
     console.log(data);
+    try {
+      await axios.post("http://localhost:3000/register", data);
+      toast.success("Dang ky thanh cong");
+    } catch (error) {
+      toast.error("something error");
+    }
   }
 
   return (
@@ -27,6 +35,7 @@ function Register() {
               required: "email is required",
             })}
           />
+          <small className="text-danger">{errors.email?.message}</small>
         </div>
         <div className="mb-3">
           <label htmlFor="exampleInputPassword1" className="form-label">
@@ -40,8 +49,8 @@ function Register() {
               required: "password is required",
             })}
           />
+          <small className="text-danger">{errors.password?.message}</small>
         </div>
-
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
