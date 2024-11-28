6eb4827d4d1de1afa9be3cb370b374db5dcc842e
vanhoa690 <vanhoa690@gmail.com>
2024-11-28 19:31:32 +0700

    luu token localstorage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,9 +10,10 @@ function Login() {
   } = useForm();
 
   async function onSubmit(data) {
-    console.log(data);
-    await axios.post("http://localhost:3000/login", data);
+    const res = await axios.post("http://localhost:3000/login", data);
     toast.success("Dang nhap thanh cong");
+    console.log(res.data);
+    localStorage.setItem("token", res.data.accessToken);
   }
   return (
     <div>
