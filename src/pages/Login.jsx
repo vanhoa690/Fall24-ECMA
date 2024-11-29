fb78788825259386fed9db05292a8264dfdbae66
vanhoa690 <vanhoa690@gmail.com>
2024-11-29 16:53:52 +0700

    add UI Login Form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,33 @@
 function Login() {
   return (
     <div>
-      <h1>Login</h1>
+      <h1 className="text-center my-2">Login</h1>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
